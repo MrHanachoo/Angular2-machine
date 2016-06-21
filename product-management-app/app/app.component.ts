b163ea9946f64ecc7b560aa406257251cf65f6ac
Mohammed Hannechi <mohammed.hannechi@gmail.com>
2016-06-21 01:25:19 +0100

    App Component

--- product-management-app/app/app.component.ts
+++ product-management-app/app/app.component.ts
@@ -1,2 +1,13 @@
+import {Component} from "angular2/core";
+
+@Component({
+  selector: "my-pm-app",
+  template: `<div>
+              <h1>{{pageTitle}}</h1>
+              <div>my first component :D</div>
+            </div>`
+})
+
 export class AppComponent {
+  pageTitle: String = "My Product Management App";
 }
