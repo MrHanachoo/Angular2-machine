c1f094caa3098ce0470a773f6b86e2c5aaf44d60
Mohammed Hannechi <mohammed.hannechi@gmail.com>
2016-07-18 00:55:27 +0100

    templates, interpolation n' directives

--- product-management-app/app/app.component.ts
+++ product-management-app/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from "angular2/core";
-
+import {ProductListComponent} from "./products/product-list.component";
 @Component({
   selector: "my-pm-app",
   template: `<div>
               <h1>{{pageTitle}}</h1>
-              <div>my first component :D</div>
-            </div>`
+              <my-pm-products>my first component :D</my-pm-products>
+            </div>`,
+  directives: [ProductListComponent]
 })
+
 
 export class AppComponent {
   pageTitle: String = "My Product Management App";
