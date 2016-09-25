a1c09ae646564e165ff3bceb0fb5a84a558d6abf
Mohammed Hannechi <mohammed.hannechi@gmail.com>
2016-09-25 17:11:31 +0100

    Register the service

--- product-management-app/app/app.component.ts
+++ product-management-app/app/app.component.ts
@@ -1,12 +1,16 @@
 import {Component} from "angular2/core";
 import {ProductListComponent} from "./products/product-list.component";
+import {ProductService} from "./products/product.service";
+
 @Component({
   selector: "my-pm-app",
   template: `<div>
               <h1>{{pageTitle}}</h1>
               <my-pm-products>my first component :D</my-pm-products>
             </div>`,
-  directives: [ProductListComponent]
+  directives: [ProductListComponent],
+  providers: [ProductService]
+
 })
 
 
